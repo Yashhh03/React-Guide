81ce9ac2e8beab0e31d01048279a970f3f0a2567
Yashhh03 <yashsurya8888@gmail.com>
2024-02-18 23:34:00 +0530

    hooks work needed

--- Basics/src/App.jsx
+++ Basics/src/App.jsx
@@ -4,9 +4,10 @@ import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
+  let tabContent = 'Please click a button';
   function handleSelect(selectedButton) {
 
-    console.log(selectedButton);
+    tabContent = selectedButton;
   }
   return (
     <div>
@@ -33,6 +34,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
+          {tabContent}
       </section>
       </main>
     </div>
