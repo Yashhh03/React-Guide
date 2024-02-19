dfab5ea3b2a1b5603d7df0ba9dd1bcc0e525280e
Yashhh03 <yashsurya8888@gmail.com>
2024-02-19 11:29:19 +0530

    select Content using hooks

--- Basics/src/App.jsx
+++ Basics/src/App.jsx
@@ -1,13 +1,14 @@
-import { CORE_CONCEPTS } from './data.js';
+import {useState} from 'react';
+
+import { CORE_CONCEPTS, EXAMPLES } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
-  let tabContent = 'Please click a button';
+  const [selectedTopic, setSelectedTopic] = useState();
   function handleSelect(selectedButton) {
-
-    tabContent = selectedButton;
+    setSelectedTopic(selectedButton);
   }
   return (
     <div>
@@ -34,7 +35,13 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          {tabContent}
+          {!selectedTopic ? (<p>Please select a Topic!</p>) : (<div id='tab-content'>
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>)}
       </section>
       </main>
     </div>
