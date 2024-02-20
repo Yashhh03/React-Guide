e9a97c255d9125d6c191cf1bfd67b2a06ffb13b9
Yashhh03 <yashsurya8888@gmail.com>
2024-02-20 23:14:38 +0530

    now buttons can be clicked once

--- Tic-Tac-Toe/src/components/GameBoard.jsx
+++ Tic-Tac-Toe/src/components/GameBoard.jsx
@@ -5,7 +5,15 @@ const initialGameBoard = [
     [null,null,null]
 ];
 
-export default function GameBoard({ onSelectSquare }) {
+export default function GameBoard({ onSelectSquare, turns }) {
+    let gameBoard = initialGameBoard;
+
+    for(const turn of turns) {
+        const {square, player} = turn;
+        const {row, col} = square;
+
+        gameBoard[row][col] = player;
+    }
 
     // const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
@@ -24,7 +32,7 @@ export default function GameBoard({ onSelectSquare }) {
         {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
             <ol>
                 {row.map((playerSymbol,colIndex) => <li key={colIndex}>
-                    <button onClick={onSelectSquare}>{playerSymbol}</button>
+                    <button onClick={() => onSelectSquare(rowIndex,colIndex)} disabled={playerSymbol !== null}>{playerSymbol}</button>
                 </li>)}
             </ol>
         </li>)}
