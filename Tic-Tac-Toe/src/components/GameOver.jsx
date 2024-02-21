1fd5c7deb87593e522c4e69fbe9804351d56c18a
Yashhh03 <yashsurya8888@gmail.com>
2024-02-21 17:49:12 +0530

    done with final changes

--- Tic-Tac-Toe/src/components/GameOver.jsx
+++ Tic-Tac-Toe/src/components/GameOver.jsx
@@ -1,13 +1,13 @@
 
 
-export default function GameOver({winner}) {
+export default function GameOver({winner, onRestart}) {
     return (
         <div id="game-over">
         <h2>Game Over!</h2>
         {winner && <p>{winner} Won</p>}
         {!winner && <p>Its a Draw</p>}
         <p>
-        <button>Rematch!</button>
+        <button onClick={onRestart}>Rematch!</button>
         </p>
         </div>
     );
