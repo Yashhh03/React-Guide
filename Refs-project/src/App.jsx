bf6e41ac60a5cb8bf6b3dac2019af458a8d28c11
Yashhh03 <yashsurya8888@gmail.com>
2024-02-25 20:13:05 +0530

    add project work functioning done

--- Refs-project/src/App.jsx
+++ Refs-project/src/App.jsx
@@ -1,11 +1,36 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectSideBar from "./components/ProjectSideBar";
 
 function App() {
+
+  const [porjectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    porjects: []
+  });
+
+  function handleStartAddProject() {
+    setProjectsState(prevState => {
+      return{
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if(porjectsState.selectedProjectId === null) {
+    content = <NewProject />
+  }else if (porjectsState.selectedProjectId === undefined){
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSideBar />
-      <NewProject />
+      <ProjectSideBar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
