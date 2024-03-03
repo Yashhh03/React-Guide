9d55494e0bced3f309fedb3c07ae690331a146d3
Yashhh03 <yashsurya8888@gmail.com>
2024-03-03 09:01:27 +0530

    added useEffect for Moda;

--- PlacePicker/src/components/Modal.jsx
+++ PlacePicker/src/components/Modal.jsx
@@ -1,13 +1,19 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
-function Modal({ open, children }) {
+function Modal({ open, children, onClose }) {
   const dialog = useRef();
 
-  dialog.current.showModal();
+  useEffect(() => {
+    if(open) {
+      dialog.current.showModal();
+    }else {
+      dialog.current.close();
+    }
+  }, [open]);
 
   return createPortal(
-    <dialog className="modal" ref={dialog} >
+    <dialog className="modal" ref={dialog} onClose={onClose} >
       {children}
     </dialog>,
     document.getElementById('modal')
