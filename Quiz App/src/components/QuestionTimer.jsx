228f3adf09081ab8be9f669c9432bddcb5bd8376
Yashhh03 <yashsurya8888@gmail.com>
2024-03-05 20:40:48 +0530

    done with all the timeout changes

--- Quiz App/src/components/QuestionTimer.jsx
+++ Quiz App/src/components/QuestionTimer.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 
-export default function QuestionTimer({timeout, onTimeout}) {
+export default function QuestionTimer({timeout, onTimeout, mode}) {
     
     const [remainingTime, setRemainingTime] = useState(timeout);
 
@@ -23,6 +23,6 @@ export default function QuestionTimer({timeout, onTimeout}) {
 
     return(
 
-        <progress id="question-time" max={timeout} value={remainingTime} />
+        <progress id="question-time" max={timeout} value={remainingTime} className={mode} />
     );
 }
--- Quiz App/src/components/Quiz.jsx
+++ Quiz App/src/components/Quiz.jsx
@@ -5,30 +5,16 @@ import imgComp from '../assets/quiz-complete.png'
 import Question from "./Question.jsx";
 
 export default function Quiz() {
-    const [answerState, setAnswerState] = useState('');
     const [userAnswers , setUserAnswers] = useState([]);
 
-    const activeQuestionIndex = answerState === '' ? userAnswers.length : userAnswers.length - 1;
+    const activeQuestionIndex = userAnswers.length;
     const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
     const handleSelectedAnswer = useCallback(function handleSelectedAnswer(selectedAnswer) {
-        setAnswerState('answered');
         setUserAnswers((prevUserAnswer) => {
             return [...prevUserAnswer,selectedAnswer];
         });
-
-        setTimeout(() => {
-            if(selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
-                setAnswerState('correct');
-            }else {
-                setAnswerState('wrong');
-            }
-
-            setTimeout(() => {
-                setAnswerState('');
-            },2000);
-        },1000);
-    },[activeQuestionIndex]);
+    },[]);
 
     const handleSkipAnswer = useCallback(() => handleSelectedAnswer(null),[handleSelectedAnswer]);
 
@@ -43,10 +29,7 @@ export default function Quiz() {
         <div id="quiz">
         <Question 
             key={activeQuestionIndex}
-            questionText={QUESTIONS[activeQuestionIndex].text} 
-            answers={QUESTIONS[activeQuestionIndex].answers} 
-            answerState={answerState}
-            selectedAnswer={userAnswers[userAnswers.length - 1]}
+            index={activeQuestionIndex}
             onSelectAnswer={handleSelectedAnswer}
             onSkipAnswer={handleSkipAnswer}    
         />
