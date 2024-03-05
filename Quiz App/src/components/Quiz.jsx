1e119dc64851487bd4bcde5c0072e333efefff04
Yashhh03 <yashsurya8888@gmail.com>
2024-03-05 17:39:40 +0530

    added questions to the quiz

--- Quiz App/src/components/Quiz.jsx
+++ Quiz App/src/components/Quiz.jsx
@@ -1,12 +1,31 @@
 import { useState } from "react";
 
+import QUESTIONS from '../questions.js';
+
 
 export default function Quiz() {
-    cont [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
-
     const [userAnswers , setUserAnswers] = useState([]);
 
+    const activeQuestionIndex = userAnswers.length;
+
+    function handleSelectedAnswer(selectedAnswer) {
+        setUserAnswers((prevUserAnswer) => {
+            return [...prevUserAnswer,selectedAnswer];
+        });
+    };
+
     return(
-        <p>Currently active question</p>
+        <div id="quiz">
+        <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+            {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+                <li key={answer} className="answer">
+                    <button onClick={() => handleSelectedAnswer(answer)}>{answer}</button>
+                </li>
+            ))}
+        </ul>
+        </div>
+        </div>
     );
 }
