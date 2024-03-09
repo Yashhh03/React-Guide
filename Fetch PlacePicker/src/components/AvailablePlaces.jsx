21698df5ff14430c2cc854b80dc788aebd225739
Yashhh03 <yashsurya8888@gmail.com>
2024-03-09 21:49:21 +0530

    Done with final changes

--- Fetch PlacePicker/src/components/AvailablePlaces.jsx
+++ Fetch PlacePicker/src/components/AvailablePlaces.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
 import Error from "./Error.jsx";
+import { sortPlacesByDistance } from "../loc.js";
+import { fetchAvailablePlaces } from "../http.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState();
@@ -12,19 +14,21 @@ export default function AvailablePlaces({ onSelectPlace }) {
       setIsFetching(true);
 
       try {
-        const response = await fetch("http://localhost:3000/placesss");
-        const resData = await response.json();
+        const places = await fetchAvailablePlaces();
 
-        if (!response.ok) {
-          throw new Error("Failed to load Images");
-        }
-
-        setAvailablePlaces(resData.places);
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortPlaces = sortPlacesByDistance(
+            places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortPlaces);
+          setIsFetching(false);
+        });
       } catch (error) {
         setError({ message: error.message || "Could not fetch Places" });
+        setIsFetching(false);
       }
-
-      setIsFetching(false);
     }
 
     fetchPlaces();
---
+++ Fetch PlacePicker/src/http.js
@@ -0,0 +1,37 @@
+export async function fetchAvailablePlaces() {
+  const response = await fetch("http://localhost:3000/places");
+  const resData = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Failed to load Images");
+  }
+  return resData.places;
+}
+
+export async function fetchUserPlaces() {
+  const response = await fetch("http://localhost:3000/user-places");
+  const resData = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Failed to load User Images");
+  }
+  return resData.places;
+}
+
+export async function updateUserPlaces(places) {
+  const response = await fetch("http://localhost:3000/user-places", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ places }),
+  });
+
+  const resData = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Failed to update User Places");
+  }
+
+  return resData.message;
+}
