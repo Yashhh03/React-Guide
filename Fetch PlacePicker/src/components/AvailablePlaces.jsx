222dd004e02b20f86901f96f5a2f9b1c5c676e30
Yashhh03 <yashsurya8888@gmail.com>
2024-03-09 20:04:15 +0530

    add error message

--- Fetch PlacePicker/src/components/AvailablePlaces.jsx
+++ Fetch PlacePicker/src/components/AvailablePlaces.jsx
@@ -1,10 +1,45 @@
-import Places from './Places.jsx';
+import { useState, useEffect } from "react";
+import Places from "./Places.jsx";
+import Error from "./Error.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState();
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetching(true);
+
+      try {
+        const response = await fetch("http://localhost:3000/placesss");
+        const resData = await response.json();
+
+        if (!response.ok) {
+          throw new Error("Failed to load Images");
+        }
+
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({ message: error.message || "Could not fetch Places" });
+      }
+
+      setIsFetching(false);
+    }
+
+    fetchPlaces();
+  }, []);
+
+  if (error) {
+    return <Error title="An error occurred!" message={error.message} />;
+  }
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching Place Data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
