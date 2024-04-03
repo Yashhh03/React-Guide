f17deaff27aa2a986f577d98d5ebce52da5056d4
Yashhh03 <yashsurya8888@gmail.com>
2024-04-03 23:06:30 +0530

    done with basic auth

--- Auth in React App/frontend/src/pages/Authentication.js
+++ Auth in React App/frontend/src/pages/Authentication.js
@@ -37,6 +37,10 @@ export async function action({ request, params }) {
     throw json({ message: "Could not auth user." }, { status: 500 });
   }
 
-  //..
+  const resData = await response.json();
+  const token = resData.token;
+
+  localStorage.setItem("token", token);
+
   return redirect("/");
 }
