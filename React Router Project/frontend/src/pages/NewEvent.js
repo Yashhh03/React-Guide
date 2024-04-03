23d3f031fd4cec9ce32f8700cf76369ce62863a6
Yashhh03 <yashsurya8888@gmail.com>
2024-04-03 13:06:36 +0530

    unfinished project

--- React Router Project/frontend/src/pages/NewEvent.js
+++ React Router Project/frontend/src/pages/NewEvent.js
@@ -1,3 +1,4 @@
+import { json, redirect } from "react-router-dom";
 import EventForm from "../components/EventForm";
 
 function NewEventPage() {
@@ -5,3 +6,28 @@ function NewEventPage() {
 }
 
 export default NewEventPage;
+
+export async function action({ request, params }) {
+  const data = await request.formData();
+
+  const eventData = {
+    title: data.get("title"),
+    image: data.get("image"),
+    description: data.get("description"),
+    date: data.get("date"),
+  };
+
+  const response = await fetch("http://localhost:8080/events", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.string(eventData),
+  });
+
+  if (!response.ok) {
+    throw json({ message: "Could not save event" }, { status: 500 });
+  }
+
+  return redirect("/events");
+}
