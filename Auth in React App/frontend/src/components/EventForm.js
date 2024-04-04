3bbd74345ac7cd5a1545dcbc75e7d64882245200
Yashhh03 <yashsurya8888@gmail.com>
2024-04-04 21:55:52 +0530

    done with basic auth

--- Auth in React App/frontend/src/components/EventForm.js
+++ Auth in React App/frontend/src/components/EventForm.js
@@ -101,7 +101,6 @@ export async function action({ request, params }) {
     const eventId = params.eventId;
     url = "http://localhost:8080/events/" + eventId;
   }
-
   const token = getAuthToken();
   const response = await fetch(url, {
     method: method,
