01fdbd744518a37e88bddc1591c36420cfd5bbf0
Yashhh03 <yashsurya8888@gmail.com>
2024-04-08 22:01:34 +0530

    populated some data in the pages

--- Full Stack Restaurant/food-restaurant/app/meals/page.js
+++ Full Stack Restaurant/food-restaurant/app/meals/page.js
@@ -1,3 +1,23 @@
+import MealsGrid from "@/components/meals/meals-grid";
+import classes from "./page.module.css";
+import Link from "next/link";
+
 export default function MealsPage() {
-  return <h1>Meals Page</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious Meal, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Choose your favorite recipes and cook it yourself and have fun.</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favorite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 }
