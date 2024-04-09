d53d5e2720a1ada6a05d739d54dc00ab3f23a7dc
Yashhh03 <yashsurya8888@gmail.com>
2024-04-09 07:43:51 +0530

    added database and fetching the data

--- Full Stack Restaurant/food-restaurant/app/meals/page.js
+++ Full Stack Restaurant/food-restaurant/app/meals/page.js
@@ -1,6 +1,14 @@
 import MealsGrid from "@/components/meals/meals-grid";
 import classes from "./page.module.css";
 import Link from "next/link";
+import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
+
+async function Meals() {
+  const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+}
 
 export default function MealsPage() {
   return (
@@ -16,7 +24,11 @@ export default function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching Meals..</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
