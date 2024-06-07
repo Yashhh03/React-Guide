85853b9e2fdfa7444df8f0a63cc844491c477ac3
Yashhh03 <yashsurya8888@gmail.com>
2024-06-07 10:45:23 +0530

    Done working with Modal

--- Revision/src/components/NewPost.jsx
+++ Revision/src/components/NewPost.jsx
@@ -1,28 +1,47 @@
+import { useState } from "react";
+
 import classes from "./NewPost.module.css";
 
-export default function NewPost(props) {
+function NewPost({ onCancel }) {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+    const postData = {
+      body: enteredBody,
+      author: enteredAuthor,
+    };
+    console.log(postData);
+    onCancel();
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea
-          name="body"
-          id="body"
-          required
-          rows={3}
-          onChange={props.onBodyChange}
-        ></textarea>
+        <textarea id="body" required rows={3} onChange={bodyChangeHandler} />
       </p>
-      {/* <p>{enteredBody}</p> */}
       <p>
-        <label htmlFor="name">Your Name</label>
-        <textarea
-          type="text"
-          id="name"
-          required
-          onChange={props.onAuthorChange}
-        ></textarea>
+        <label htmlFor="name">Your name</label>
+        <input type="text" id="name" required onChange={authorChangeHandler} />
+      </p>
+      <p className={classes.actions}>
+        <button type="button" onClick={onCancel}>
+          Cancel
+        </button>
+        <button>Submit</button>
       </p>
     </form>
   );
 }
+
+export default NewPost;
